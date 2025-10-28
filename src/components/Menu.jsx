81de639db3c79ed7d789ac941c398edddd0a4638
Maxi <maximiliano.zavala@davinci.edu.ar>
2025-10-28 18:20:54 -0300

    Menu: add logo

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -6,11 +6,10 @@ const Menu = () => {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container>
-                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Brand href="/">Auto-Check</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="me-auto">
-                    <Nav.Link as={Link} to="/">Home</Nav.Link>
                     <Nav.Link as={Link} to="perfil">Perfil</Nav.Link>
                     <Nav.Link as={Link} to="garage">Garage</Nav.Link>
                 </Nav>
