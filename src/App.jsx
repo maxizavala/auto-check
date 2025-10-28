60ebcd09b9faa68777a2c6f8a5b024444a9d6d89
Maxi Bleu <appleprime@MacBook-Pro-de-Maxi.local>
2025-10-28 18:10:43 -0300

    Menu + react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import { Container } from 'react-bootstrap';
-import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Menu from './components/Menu';
+import Garage from './pages/Garage';
 
 
 const App = () => {
 
 	return (
-		<Container className="texto-centrado">
-            <h1>AutoCheck</h1>
-		</Container>
-  )
+        <BrowserRouter>
+            <Menu />
+            <Container>
+                <Routes>
+                    <Route path="/" element={ <h1> Pagina principal </h1> } />
+                    <Route path="/garage" element={ <Garage /> } />
+                    <Route path="*" element={ <h1> Pagina Inexistente </h1> } />
+                </Routes>
+            </Container>
+        </BrowserRouter>
+    )
 }
 
 export default App
---
+++ src/components/Menu.jsx
@@ -0,0 +1,22 @@
+import { Container, Nav, Navbar } from "react-bootstrap"
+import { Link, useNavigate } from 'react-router-dom';
+
+
+const Menu = () => {
+    return (
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="me-auto">
+                    <Nav.Link as={Link} to="/">Home</Nav.Link>
+                    <Nav.Link as={Link} to="pokeapi">Pokemons</Nav.Link>
+                </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    )
+}
+
+export default Menu
